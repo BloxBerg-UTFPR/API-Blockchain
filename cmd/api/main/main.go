b80f09702a85c9826211f366c65c11b3b210149c
Felipe Stawinski Suzuki <fsuzuki@alunos.utfpr.edu.br>
2025-05-30 19:22:29 -0300

    added jwttoken

--- cmd/api/main/main.go
+++ cmd/api/main/main.go
@@ -13,7 +13,7 @@ func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, jwttoken, jwtToken")
 
 		// Handle preflight requests
 		if r.Method == http.MethodOptions {
